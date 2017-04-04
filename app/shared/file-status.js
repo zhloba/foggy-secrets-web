10388153439de44a939af8f821304aa486d889e6
Alexander Zhloba <AZhloba@VIACODE.COM>
2017-04-04 21:17:06 +0300

    deleted js files

--- app/shared/file-status.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(function (FileStatus) {
-    FileStatus[FileStatus["Encrypting"] = 0] = "Encrypting";
-    FileStatus[FileStatus["Encrypted"] = 1] = "Encrypted";
-    FileStatus[FileStatus["Decrypting"] = 2] = "Decrypting";
-    FileStatus[FileStatus["Decrypted"] = 3] = "Decrypted";
-})(exports.FileStatus || (exports.FileStatus = {}));
-var FileStatus = exports.FileStatus;
-//# sourceMappingURL=file-status.js.map
