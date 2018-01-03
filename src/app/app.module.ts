7223a89fa998d11095e8e117bd02124167388d0a
Alexander <zhlobale@gmail.com>
2018-01-04 02:06:32 +0300

    added password validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
 import { UploadFormComponent } from './upload-form/upload-form.component'
 import { DragAndDropUploadDirective } from './upload-form/drag-and-drop-upload.directive'
@@ -13,7 +13,8 @@ import { EncodingService } from './shared/encoding.service'
 @NgModule({
     imports: [
          BrowserModule,
-         FormsModule 
+         FormsModule,
+         ReactiveFormsModule 
     ],
     declarations: [
          AppComponent,
--- src/app/upload-form/upload-form.component.ts
+++ src/app/upload-form/upload-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { FileService } from '../shared/file.service'
 
 @Component({
@@ -7,13 +8,47 @@ import { FileService } from '../shared/file.service'
     templateUrl: 'upload-form.component.html',
     styleUrls: ['upload-form.component.css']
 })
-export class UploadFormComponent {
-    password: string = '';
-    constructor(private fileService: FileService) {
+export class UploadFormComponent implements OnInit {
+    form: FormGroup;
+    constructor(private fileService: FileService, private formBuilder: FormBuilder) {
 
+    }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            password: [null, Validators.required]
+        });
+    }
+
+    isFieldValid(field: string) {
+        return !this.form.get(field).valid && this.form.get(field).touched;
+    }
+
+    displayFieldCss(field: string) {
+        return {
+            'has-error': this.isFieldValid(field),
+            'has-feedback': this.isFieldValid(field)
+        };
+    }
+
+    validateAllFormFields(formGroup: FormGroup) {         
+        Object.keys(formGroup.controls).forEach(field => {  
+            const control = formGroup.get(field);             
+            if (control instanceof FormControl) {             
+                control.markAsTouched({ onlySelf: true });
+            } else if (control instanceof FormGroup) {        
+                this.validateAllFormFields(control);            
+            }
+        });
     }
 
     onAdd(files: File[]) {
-        this.fileService.addFile(files, this.password);
+        if (this.form.valid) {
+            this.fileService.addFile(files, this.form.value.password);
+        }
+        else {
+            this.validateAllFormFields(this.form);
+        }
     }
+
 }
